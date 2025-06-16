faa684a0e98e78f42830710534b9bb67a75a472f
unknown <tov.kessler@yandex.ru>
2025-06-17 02:54:32 +0300

    feat: add header

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,13 +1,57 @@
 // Автоскейл
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(() => {
   function autoscale() {
-    const breakpoint = window.innerWidth > 1440 ? window.innerWidth : window.innerWidth > 640 ? 1440 : window.innerWidth > 320 ? 640 : 320; 
+    const breakpoint =
+      window.innerWidth > 1440
+        ? window.innerWidth
+        : window.innerWidth > 640
+        ? 1440
+        : window.innerWidth > 320
+        ? 640
+        : 320;
     const zoom = window.innerWidth / breakpoint;
 
     document.documentElement.style.zoom = zoom;
   }
-  
-  window.addEventListener("resize", autoscale);
-  window.addEventListener("load", autoscale);
 
+  $(window).on("resize", autoscale);
+  $(window).on("load", autoscale);
+});
+
+// Хедер
+$(document).ready(() => {
+  $(".header .burger").on("click", () => {
+    $("body").toggleClass("menu-open");
+    $(".nav__item").removeClass("active");
+    $(".nav__submenu").hide(300);
+  });
+
+  $(".nav__item_submenu .nav__link").on("click", (e) => {
+    if (window.innerWidth > 640) {
+      return;
+    }
+
+    const parent = $(e.currentTarget).parent();
+    const isActive = parent.hasClass("active");
+
+    if (isActive) {
+      parent.removeClass("active");
+      parent.find(".nav__submenu").hide(300);
+    } else {
+      $(".nav__item").removeClass("active");
+      $(".nav__submenu").hide(300);
+
+      parent.addClass("active");
+      parent.find(".nav__submenu").show(300);
+    }
+  });
+
+  $(window).on("resize", () => {
+    if (window.innerWidth > 640) {
+      $("body").removeClass("menu-open");
+      $(".nav__item").removeClass("active");
+      $(".nav__submenu").hide(300);
+      $(".nav__item").removeClass("active");
+    }
+  });
 });
